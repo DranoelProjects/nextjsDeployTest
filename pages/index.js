9919194936548a9b52ce25e06ebb63efe9f3977b
Wambergue Léonard <leonard.wamberguepro@gmail.com>
2023-09-23 20:37:10 +0200

    deployment test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Landing page</title>
+        <title>Landing page test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
